b152532dab55d8b4879d74a9b2036aeca72bddce
Daocheng Su <18392441481@163.com>
2020-07-22 22:21:23 +0800

    third day

--- ToyReact.js
+++ ToyReact.js
@@ -1,42 +1,69 @@
 class ElementWrapper {
 	constructor(type) {
-		this.root = document.createElement(type);
+		this.type = type;
+		this.props = Object.create(null);
+		this.children = [];
 	}
 	setAttribute(name, value) {
-		if (name.match(/^on([\s\S]+)$/)) {
-			let eventname = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
-			this.root.addEventListener(eventname, value);
-			return;
-		}
-		if (name == 'className') {
-			name = 'class';
-		}
-		this.root.setAttribute(name, value);
+		this.props[name] = value;
 	}
 	appendChild(vchild) {
-		let range = document.createRange();
-		if (this.root.children.length) {
-			range.setStartAfter(this.root.lastChild);
-			range.setEndAfter(this.root.lastChild);
-		} else {
-			range.setStart(this.root, 0);
-			range.setEnd(this.root, 0);
-		}
-		vchild.mountTo(range);
+		this.children.push(vchild.vdom);
+	}
+	get vdom() {
+		return this;
 	}
 	mountTo(range) {
+		this.range = range;
+		let placeHolder = document.createComment('placeHolder');
+		let endRange = document.createRange();
+		endRange.setStart(this.range.endContainer, this.range.endOffset);
+		endRange.setEnd(this.range.endContainer, this.range.endOffset);
+		endRange.insertNode(placeHolder);
 		range.deleteContents();
-		range.insertNode(this.root);
+		let element = document.createElement(this.type);
+		for (let name in this.props) {
+			let value = this.props[name];
+			if (name.match(/^on([\s\S]+)$/)) {
+				let eventname = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
+				element.addEventListener(eventname, value);
+				continue;
+			}
+			if (name === 'className') {
+				element.setAttribute('class', value);
+			}
+
+			element.setAttribute(name, value);
+		}
+		for (let child of this.children) {
+			let range = document.createRange();
+			if (element.children.length) {
+				range.setStartAfter(element.lastChild);
+				range.setEndAfter(element.lastChild);
+			} else {
+				range.setStart(element, 0);
+				range.setEnd(element, 0);
+			}
+			child.mountTo(range);
+		}
+		range.insertNode(element);
 	}
 }
 
 class TextWrapper {
 	constructor(content) {
 		this.root = document.createTextNode(content);
+		this.type = '#text';
+		this.children = [];
+		this.props = Object.create(null);
 	}
 	mountTo(range) {
+		this.range = range;
 		range.deleteContents();
 		range.insertNode(this.root);
+	}
+	get vdom() {
+		return this;
 	}
 }
 
@@ -45,19 +72,81 @@ export class Component {
 		this.children = [];
 		this.props = Object.create(null);
 	}
+	get type() {
+		return this.constructor.name;
+	}
 	mountTo(range) {
 		this.range = range;
 		this.update();
 	}
 	update() {
-		let placeHolder = document.createComment('placeHolder');
-		let range = document.createRange();
-		range.setStart(this.range.endContainer, this.range.endOffset);
-		range.setEnd(this.range.endContainer, this.range.endOffset);
-		range.insertNode(placeHolder);
-		this.range.deleteContents();
-		var vdom = this.render();
-		vdom.mountTo(this.range);
+		let vdom = this.vdom;
+		if (this.oldVdom) {
+			let isSameNode = (node1, node2) => {
+				if (node1.type != node2.type) {
+					return false;
+				}
+				for (let name in node1.props) {
+					// if (
+					// 	typeof node1.props[name] == 'function' &&
+					// 	typeof node2.props[name] === 'function' &&
+					// 	node1.props[name].toString() === node2.props[name].toString()
+					// ) {
+					// 	continue;
+					// }
+					if (
+						typeof node1.props[name] == 'object' &&
+						typeof node2.props[name] === 'object' &&
+						JSON.stringify(node1.props[name]) === JSON.stringify(node2.props[name])
+					) {
+						continue;
+					}
+					if (node1.props[name] !== node2.props[name]) {
+						return false;
+					}
+				}
+				if (Object.keys(node1.props).length !== Object.keys(node2.props).length) {
+					return false;
+				}
+				return true;
+			};
+			let isSameTree = (node1, node2) => {
+				if (!isSameNode(node1, node2)) {
+					return false;
+				}
+				if (node1.children.length !== node2.children.length) {
+					return false;
+				}
+				for (let i = 0; i < node1.children.length; i++) {
+					if (!isSameTree(node1.children[i], node2.children[i])) {
+						return false;
+					}
+				}
+				return true;
+			};
+			if (isSameTree(vdom, this.oldVdom)) {
+				return;
+			}
+			let replace = (newTree, oldTree) => {
+				if (isSameTree(newTree, oldTree)) {
+					return;
+				}
+				if (!isSameNode(newTree, oldTree)) {
+					newTree.mountTo(oldTree.range);
+				} else {
+					for (let i = 0; i < newTree.children.length; i++) {
+						replace(newTree.children[i], oldTree.children[i]);
+					}
+				}
+			};
+			replace(vdom, this.oldVdom);
+		} else {
+			vdom.mountTo(this.range);
+		}
+		this.oldVdom = vdom;
+	}
+	get vdom() {
+		return this.render().vdom;
 	}
 	setAttribute(name, value) {
 		this.props[name] = value;
@@ -69,11 +158,15 @@ export class Component {
 	setState(state) {
 		let merge = (oldstate, newstate) => {
 			for (let p in newstate) {
-				if (typeof newstate[p] === 'object') {
+				if (typeof newstate[p] === 'object' && newstate[p] !== null) {
 					if (typeof oldstate[p] !== 'object') {
-						oldstate[p] = {};
-						merge(oldstate[p], newstate[p]);
+						if (newstate[p] instanceof Array) {
+							oldstate[p] = [];
+						} else {
+							oldstate[p] = {};
+						}
 					}
+					merge(oldstate[p], newstate[p]);
 				} else {
 					oldstate[p] = newstate[p];
 				}
@@ -100,6 +193,9 @@ export let ToyReact = {
 		}
 		let insertChildren = (children) => {
 			for (const child of children) {
+				if (child == null || child == void 0) {
+					child = '';
+				}
 				if (typeof child === 'object' && child instanceof Array) {
 					insertChildren(child);
 				} else {
